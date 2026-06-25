e5fd47eff951e22a1dcaa7524c7d4b4496605f52
AndreMikulec <Andre_Mikulec@Hotmail.com>
2026-06-25 14:41:15 -0400

    build.sh --enable-shared AND  ls -alrt -R $(cygpath ${GITHUB_WORKSPACE})
    build.sh --enable-shared
    ls -alrt -R $(cygpath ${GITHUB_WORKSPACE})

--- msys2/build.sh
+++ msys2/build.sh
@@ -48,6 +48,7 @@ PKG_CONFIG_PATH=/clang64/ffbuild/lib/pkgconfig ./configure \
     --extra-ldflags=-L/clang64/ffbuild/lib \
     --prefix=/clang64/ffbuild/jellyfin-ffmpeg \
     --extra-version=Jellyfin \
+    --enable-shared \
     --disable-unstable \
     --disable-ffplay \
     --disable-debug \
@@ -110,6 +111,10 @@ while IFS= read -r line; do
         fi
     fi
 done < "$BUILDER_ROOT"/../debian/changelog
+
+echo "Begin Recursion"
+ls -alrt -R $(cygpath ${GITHUB_WORKSPACE})
+echo "End Recursion"
 
 PKG_NAME="jellyfin-ffmpeg_${PKG_VER}_portable_${TARGET}-${VARIANT}${ADDINS_STR:+-}${ADDINS_STR}"
 ARTIFACTS_PATH="$BUILDER_ROOT"/artifacts
