c045e75402f66ad04ac68f76454af01f4a6cecd0
AndreMikulec <Andre_Mikulec@Hotmail.com>
2026-06-29 17:27:27 -0400

    build.sh - No Disable Debug
    Removed ---disable-debug

--- msys2/build.sh
+++ msys2/build.sh
@@ -49,7 +49,6 @@ PKG_CONFIG_PATH=/clang64/ffbuild/lib/pkgconfig ./configure \
     --prefix=/clang64/ffbuild/jellyfin-ffmpeg \
     --extra-version=Jellyfin \
     --disable-unstable \
-    --disable-debug \
     --disable-doc \
     --enable-lto=thin \
     --enable-gpl \
